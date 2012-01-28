8a86f678d1db75cae825c43485d0bb63c13b2ee7
Olexandr Romanovskyi <bigroma73@gmail.com>
2012-01-28 15:14:55 +0200

    add converters and renders

---
+++ Components/Converter/CamdInfo3.py
@@ -0,0 +1,129 @@
+#
+#  CamdInfo - Converter
+#
+#  Coded by weazle (c) 2010
+#  Support: www.dreambox-tools.info
+#
+#  This plugin is licensed under the Creative Commons 
+#  Attribution-NonCommercial-ShareAlike 3.0 Unported 
+#  License. To view a copy of this license, visit
+#  http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative
+#  Commons, 559 Nathan Abbott Way, Stanford, California 94305, USA.
+#
+#  Alternatively, this plugin may be distributed and executed on hardware which
+#  is licensed by Dream Multimedia GmbH.
+
+#  This plugin is NOT free software. It is open source, you are allowed to
+#  modify it (if you keep the license), but it may not be commercially 
+#  distributed other than under the conditions noted above.
+# mod by 2boom 2011-2012
+
+# <widget source="session.CurrentService" render="Label" position="189,397" zPosition="4" size="350,20" noWrap="1" valign="center" halign="center" font="Regular;14" foregroundColor="clText" transparent="1"  backgroundColor="#20002450">
+#	<convert type="CamdInfo">Camd</convert>
+# </widget>			
+
+from enigma import iServiceInformation
+from Components.Converter.Converter import Converter
+from Components.Element import cached
+from Tools.Directories import fileExists
+import os
+
+
+class CamdInfo3(Converter, object):
+	def __init__(self, type):
+		Converter.__init__(self, type)
+
+	@cached
+	def getText(self):
+		service = self.source.service
+		info = service and service.info()
+		camd = ""
+		if not info:
+			return ""
+		# VTI 	
+		if fileExists("/tmp/.emu.info"):
+			try:
+				camdlist = open("/tmp/.emu.info", "r")
+			except:
+				return None
+		# BlackHole	
+		elif fileExists("/etc/CurrentBhCamName"):
+			try:
+				camdlist = open("/etc/CurrentBhCamName", "r")
+			except:
+				return None
+		# Domica	
+		elif fileExists("/etc/active_emu.list"):
+			try:
+				camdlist = open("/etc/active_emu.list", "r")
+			except:
+				return None
+		# Egami	
+		elif fileExists("/tmp/egami.inf","r"):
+			lines = open("/tmp/egami.inf", "r").readlines()
+			for line in lines:
+				item = line.split(":",1)
+				if item[0] == "Current emulator":
+					return item[1].strip()
+		#Pli
+		elif fileExists("/etc/init.d/softcam") or fileExists("/etc/init.d/cardserver"):
+			try:
+				camdlist = os.popen("/etc/init.d/softcam info")
+			except:
+				pass
+			try:
+				serlist = os.popen("/etc/init.d/cardserver info")
+			except:
+				pass
+			
+		# OoZooN
+		elif fileExists("/tmp/cam.info"):
+			try:
+				camdlist = open("/tmp/cam.info", "r")
+			except:
+				return None
+		# Merlin2	
+		elif fileExists("/etc/clist.list"):
+			try:
+		   		camdlist = open("/etc/clist.list", "r")
+		   	except:
+				return None
+		# GP3
+		elif fileExists("/usr/lib/enigma2/python/Plugins/Bp/geminimain/lib/libgeminimain.so"):
+			try:
+				from Plugins.Bp.geminimain.plugin import GETCAMDLIST
+				from Plugins.Bp.geminimain.lib import libgeminimain
+				camdl = libgeminimain.getPyList(GETCAMDLIST)
+				cam = None
+				for x in camdl:
+					if x[1] == 1:
+						cam = x[2] 
+				return cam
+		   	except:
+				return None
+		# Unknown emu
+		else:
+			return None
+			
+		if serlist is not None:
+			cardserver = ""
+			for current in serlist.readlines():
+				cardserver = current
+			serlist.close()
+		else:
+			cardserver = "NA"
+
+		if camdlist is not None:
+			emu = ""
+			for current in camdlist.readlines():
+				emu = current
+			camdlist.close()
+		else:
+			emu = "NA"
+			
+		return "%s %s" % (cardserver.split('\n')[0], emu.split('\n')[0])
+		
+	text = property(getText)
+
+	def changed(self, what):
+		Converter.changed(self, what)
